57a56f1ffcc879817d95459d80ea552014c3868d
Ruslan Kurbanov <wkurbanov@gmail.com>
2016-12-29 23:47:38 +0300

    first commit

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,15 +1,22 @@
 import vk
+from getpass import getpass
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
-
+APP_ID = 5779418  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
 
 def get_user_login():
-    pass
+    login = None
+    login = input('Введите логин: ')
+    while not login:
+        login = input('Пустой поле, введите логин повторно: ')
+    return login
 
 
 def get_user_password():
-    pass
+    password = None
+    password = getpass('Введите пароль: ')
+    while not password:
+        password = getpass('Пустой поле, введите пароль повторно: ')
+    return password
 
 
 def get_online_friends(login, password):
@@ -17,13 +24,20 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
     api = vk.API(session)
-    # например, api.friends.get()
+    online_friends = api.friends.getOnline()
+    return online_friends
 
 
 def output_friends_to_console(friends_online):
-    pass
+    if friends_online:
+        print('Друзья онлайн:')
+        for friend in friends_online:
+            print(friend['first_name'], friend['last_name'])
+    else:
+        print('Нет друзей онлайн')
 
 if __name__ == '__main__':
     login = get_user_login()
